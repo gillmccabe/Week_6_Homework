e2951fa81fbca1ad0b685a1154ff78859da1e4ad
Gillian McCabe <gillianmccabe@outlook.com>
2016-08-28 19:31:22 +0100

    runner set up

---
+++ weekend/game/Runner.java
@@ -0,0 +1,13 @@
+package game;
+import java.util.*;
+
+public class Runner {
+  public static void main(String[] args){
+
+    ArrayList<Player> players = new ArrayList<Player>();
+    Deck deck = new Deck();
+    Game game = new Game(deck);
+    Scanner input = new Scanner(System.in);
+
+  }
+}
