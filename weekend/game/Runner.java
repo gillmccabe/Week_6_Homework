d53fb6aa773469d84c17bd8e2037b9a23d685c46
Gillian McCabe <gillianmccabe@outlook.com>
2016-08-28 19:35:36 +0100

    add players to game runner

--- weekend/game/Runner.java
+++ weekend/game/Runner.java
@@ -9,5 +9,17 @@ public class Runner {
     Game game = new Game(deck);
     Scanner input = new Scanner(System.in);
 
+    System.out.println("Please enter Player 1 name: ");
+    String player1Name = input.next();
+    Player player1 = new Player(player1Name);
+    System.out.println("Please enter Player 2 name: ");
+    String player2Name = input.next();
+    Player player2 = new Player(player2Name);
+
+    game.addAPlayer(player1);
+    game.addAPlayer(player2);
+    game.dealToPlayers();
+
+
   }
 }
